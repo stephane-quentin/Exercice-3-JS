7d16540987103c84c7841eb8ead475eed6e7545a
Stiouf <stephane.quentin@live.fr>
2018-10-05 15:09:33 +0200

    ex3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,2 @@
-var prenom = prompt("Entrez votre prénom :");
-alert("Bonjour, " + prenom);
+var prenom = prompt("Entrez votre prénom :"); //Demande du prénom via une boite du dialogue qui est mis dans une variable
+alert("Bonjour, " + prenom); // affichage d'une boite de dialogue avec Bonjour, (ce qui est entre"") et le contenu de la variable
