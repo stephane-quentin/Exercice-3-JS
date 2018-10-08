bdf7b6e18eb2b9a8431d1a6c183a82d5affc8049
Stiouf <stephane.quentin@live.fr>
2018-10-08 10:54:55 +0200

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,9 @@
-var prenom = prompt("Entrez votre prénom :"); //Demande du prénom via une boite du dialogue qui est mis dans une variable
-alert("Bonjour, " + prenom); // affichage d'une boite de dialogue avec Bonjour, (ce qui est entre"") et le contenu de la variable
+// var prenom = prompt("Entrez votre prénom :"); //Demande du prénom via une boite du dialogue qui est mis dans une variable
+// alert("Bonjour, " + prenom); // affichage d'une boite de dialogue avec Bonjour, (ce qui est entre"") et le contenu de la variable
+var firstName = prompt ("Entrez votre prénom :")
+if(isNaN(firstName)){ //isNaN is Not a Number
+  alert('Bonjour ' + firstName);
+}
+  else {
+    alert('Erreur!');
+};
